410420c6ec5a973ce63eb23ddc45c5e5665a2fda
gaobiaoli <1258497127@qq.com>
2024-01-10 18:08:04 +0800

    0.0.1

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
     name="vUtils",
     version="0.0.1",
     author="gaobiaoli",
+    url="https://github.com/gaobiaoli/vUtils",
     author_email="gaobiaoli@tongji.edu.cn",
-    packages=["vUtils"],
+    packages=find_packages(),
 )
 
