26d9c7c6e4a1aa57f8cdfaf021b42af61c1349e8
gaobiaoli <1258497127@qq.com>
2024-07-01 21:21:10 +0800

    add MultiVideoCapture

---
+++ vUtils/capture/MultiVideoCapture.py
@@ -0,0 +1,19 @@
+from typing import List
+class MultiVideoCapture():
+    def __init__(self,capList:List) -> None:
+        self.capList = capList
+
+    def read(self):
+        frameList=[]
+        ret=True
+        for cap in self.capList:
+            subret,frame = cap.read()
+            ret =  subret & ret
+            frameList.append(frame)
+        return ret,frameList
+    
+    def count(self):
+        countList=[]
+        for cap in self.capList:
+            countList.append(cap.count())
+        return countList
