9ba266fee1250a2ff45dee5ebd046d919aa76ec3
Gao Biaoli <77002530+gaobiaoli@users.noreply.github.com>
2024-10-24 14:03:02 +0800

    Update dependency

--- setup.py
+++ setup.py
@@ -7,5 +7,10 @@ setup(
     url="https://github.com/gaobiaoli/vUtils",
     author_email="gaobiaoli@tongji.edu.cn",
     packages=find_packages(),
+    install_requires=[
+        "numpy",         
+        "opencv-python>=4.10", 
+        "opencv-contrib-python>=4.10", 
+    ],
 )
 
