84d8c849d3ff1a2282d07327cad2803542f8d191
gaobiaoli <1258497127@qq.com>
2025-05-07 15:16:54 +0800

    Regis-Homo & tests

--- tests/GetHomo.py
+++ tests/GetHomo.py
@@ -6,6 +6,7 @@ sys.path.append(ROOT)
 from vUtils.utils.RegisUtils import HomographyInit
 
 if __name__ == "__main__":
+    # 鼠标左键选取点，esc退出后自动计算单应性矩阵
     src = cv2.imread(os.path.join(ROOT,"tests/data/test_img.jpg"))
     dst = cv2.imread(os.path.join(ROOT,"tests/data/test_bim.png"))
-    HomographyInit.run(src, dst, save_path=None, show=True)
+    HomographyInit.run(src, dst, save_path=os.path.join(ROOT,"tests/data/H.npy"), show=True)
